127550f864a32de49f724211c8ea99fdcd845ad9
janisatadeesri284 <janisata.deesri.094@gmail.com>
2024-11-21 18:44:40 +0000

    password.py

---
+++ cell02/ex02/password.py
@@ -0,0 +1,10 @@
+password = "Python is awesome"
+
+# Prompt the user for a password
+user_input = input("Enter the password: ")
+
+# Check if the entered password matches the predefined one
+if user_input == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
