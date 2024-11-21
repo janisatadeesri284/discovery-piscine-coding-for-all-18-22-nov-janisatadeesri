5aec2ff6c043b101ad8bede4b7bc7940d66d68c0
janisatadeesri284 <janisata.deesri.094@gmail.com>
2024-11-21 19:53:24 +0000

    calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,10 @@
+first_number = float(input("Give me the first number: "))
+second_number = float(input("Give me the second number: "))
+
+print("Thank you!")
+
+
+print(f"{first_number} + {second_number} = {first_number + second_number}")
+print(f"{first_number} - {second_number} = {first_number - second_number}")
+print(f"{first_number} / {second_number} = {first_number / second_number}")
+print(f"{first_number} * {second_number} = {first_number * second_number}")
